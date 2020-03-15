de40b57db9438ce1267a681496c681303d657004
Herdy Handoko <herdy.handoko@gmail.com>
2020-03-15 09:38:47 +0800

    Add doc-comments for improved clarity and intent

--- src/main/scala/com/hhandoko/aktiform/AktiformApplication.scala
+++ src/main/scala/com/hhandoko/aktiform/AktiformApplication.scala
@@ -3,11 +3,17 @@ package com.hhandoko.aktiform
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
-import org.springframework.context.annotation.Import
+import org.springframework.context.annotation.{Configuration, Import}
 
 import com.hhandoko.aktiform.app.config.ResourcesConfig
 import com.hhandoko.aktiform.app.module.PolyglotContextModule
 
+/**
+ * Main application class.
+ *
+ * `EnableConfigurationProperties` and `Import` is used here to make it explicit configuration and modules that are
+ * loaded than relying on automated component scanning.
+ */
 @EnableConfigurationProperties(
   value = Array(
     classOf[ResourcesConfig]
@@ -19,6 +25,7 @@ import com.hhandoko.aktiform.app.module.PolyglotContextModule
   )
 )
 @SpringBootApplication
+@Configuration
 class AktiformApplication {}
 
 object AktiformApplication {
--- src/main/scala/com/hhandoko/aktiform/app/module/PolyglotContextModule.scala
+++ src/main/scala/com/hhandoko/aktiform/app/module/PolyglotContextModule.scala
@@ -3,6 +3,12 @@ package com.hhandoko.aktiform.app.module
 import org.graalvm.polyglot.Context
 import org.springframework.context.annotation.{Bean, Configuration}
 
+/**
+ * GraalVM polyglot feature context module loader.
+ *
+ * Creates polyglot context as a Singleton that will be passed to dependent classes, as context creation is an expensive
+ * process.
+ */
 @Configuration
 class PolyglotContextModule {
 
