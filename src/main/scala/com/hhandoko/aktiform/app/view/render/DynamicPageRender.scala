c275212840942461a57be732ba3712f411020989
Herdy Handoko <herdy.handoko@gmail.com>
2020-05-14 14:03:30 +0800

    Refactor dynamic page template loading

--- src/main/scala/com/hhandoko/aktiform/app/view/render/DynamicPageRender.scala
+++ src/main/scala/com/hhandoko/aktiform/app/view/render/DynamicPageRender.scala
@@ -4,56 +4,73 @@ import java.util.{Collections, HashMap => JHashMap}
 import scala.io.Source
 
 import com.samskivert.mustache.Mustache
+import com.samskivert.mustache.Mustache.TemplateLoader
+import com.typesafe.scalalogging.LazyLogging
 
 import com.hhandoko.aktiform.api.html.{Element, Page}
 import com.hhandoko.aktiform.app.config.ResourcesVariantConfig
 import com.hhandoko.aktiform.core.helper.AutoCloseableResource.using
 
+/** Page renderer using dynamic master template.
+  *
+  * @param config CSS variant resources configuration.
+  * @param elRender Element renderer.
+  */
 final class DynamicPageRender(
     config: ResourcesVariantConfig,
     elRender: Element => String
-) {
+) extends LazyLogging {
 
-  private final val META_TEMPLATE = "templates/section/meta.mustache"
-  private final val SCRIPTS_TEMPLATE = "templates/section/scripts.mustache"
-  private final val STYLES_TEMPLATE = "templates/section/styles.mustache"
-  private final val PAGE_TEMPLATE = "templates/dynamic.mustache"
+  /** Base template to be executed later.
+    *
+    * This template will resolve styling and script-related segments, and keep
+    * other segments (e.g. `{{content}}`) to be updated later via another
+    * Mustache template execution.
+    */
+  private final lazy val templateBase = {
+    logger.debug(
+      "[init#templateBase] Loading precached raw base template for dynamic pages"
+    )
 
-  private final val meta =
-    template(META_TEMPLATE)(Collections.EMPTY_MAP)
-
-  private final val scripts =
-    template(SCRIPTS_TEMPLATE) {
-      Collections.singletonMap("scripts", config.scripts)
+    val master = "templates/dynamic.mustache"
+    val section = (name: String) => s"templates/${name}.mustache"
+    val loader: TemplateLoader = (name: String) => {
+      Source.fromResource(section(name)).reader()
     }
+    val raw = using(Source.fromResource(master))(_.mkString)
+    val context =
+      new JHashMap[String, AnyRef] {
+        this.put("meta", Collections.EMPTY_MAP)
+        this.put("styles", config.styles)
+        this.put("scripts", config.scripts)
 
-  private final val styles =
-    template(STYLES_TEMPLATE) {
-      Collections.singletonMap("styles", config.styles)
-    }
-
-  private final val pageTemplate =
-    template(PAGE_TEMPLATE, escapeHtml = false) _
-
-  def render(page: Page): String =
-    pageTemplate {
-      new JHashMap[String, String] {
-        this.put("meta", meta)
-        this.put("styles", styles)
-        this.put("scripts", scripts)
-        this.put("content", page.els.map(elRender).mkString)
+        // Rewrap `content` segment to be executed later.
+        //
+        // Default behaviour when values are missing: `strict=true` with throw
+        // exception while `strict=false` will replace it with `false`.
+        this.put("content", "{{content}}")
       }
-    }
-
-  private[this] def template(path: String, escapeHtml: Boolean = true)(
-      context: AnyRef
-  ): String = {
-    val rawTemplate = using(Source.fromResource(path))(_.mkString)
 
     Mustache
       .compiler()
-      .escapeHTML(escapeHtml)
-      .compile(rawTemplate)
+      .withLoader(loader)
+      .compile(raw)
       .execute(context)
   }
+
+  private final lazy val template = {
+    logger.debug(
+      "[init#template] Loading executable template for dynamic pages"
+    )
+
+    Mustache
+      .compiler()
+      .escapeHTML(false)
+      .compile(templateBase)
+  }
+
+  def render(page: Page): String =
+    template.execute(
+      Collections.singletonMap("content", page.els.map(elRender).mkString)
+    )
 }
