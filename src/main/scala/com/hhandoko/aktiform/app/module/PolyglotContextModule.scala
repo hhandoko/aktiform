2bd6107b1ab5779290ade5d217c3ce7c3fe2501b
Herdy Handoko <herdy.handoko@gmail.com>
2020-05-20 17:59:56 +0800

    Add GraalVM runtime detection

--- src/main/scala/com/hhandoko/aktiform/app/module/PolyglotContextModule.scala
+++ src/main/scala/com/hhandoko/aktiform/app/module/PolyglotContextModule.scala
@@ -1,16 +1,40 @@
 package com.hhandoko.aktiform.app.module
 
+import com.typesafe.scalalogging.LazyLogging
 import org.graalvm.polyglot.Context
 import org.springframework.context.annotation.{Bean, Configuration}
+
+import com.hhandoko.aktiform.core.Capabilities
 
 /** GraalVM polyglot feature context module loader.
   *
   * Creates polyglot context as a Singleton that will be passed to dependent classes, as context creation is an expensive
   * process.
+  *
+  * TODO: To be refactored as context is not thread-safe
   */
 @Configuration
-class PolyglotContextModule {
+class PolyglotContextModule extends LazyLogging {
+
+  private final val VERSION_KEY = "java.vendor.version"
+  private final val GRAAL_VM_ID = "graalvm"
 
   @Bean
-  def context: Context = Context.create("js")
+  def context: Context = {
+    val version = System.getProperty(VERSION_KEY)
+    // TODO: Should not return null, ever. Will be refactored once workflow is
+    //       developed (alongside thread-safe Context execution).
+    if (isGraalVm(version)) {
+      logger.info(s"[context] Polyglot feature enabled, detected runtime '$version'")
+      Capabilities.polyglot = true
+      Context.create("js")
+    } else {
+      logger.warn(s"[context] Polyglot feature disabled, detected runtime is '$version'")
+      Capabilities.polyglot = false
+      null
+    }
+  }
+
+  private[this] def isGraalVm(version: String): Boolean =
+    version.trim.toLowerCase.startsWith(GRAAL_VM_ID)
 }
---
+++ src/main/scala/com/hhandoko/aktiform/core/Capabilities.scala
@@ -0,0 +1,10 @@
+package com.hhandoko.aktiform.core
+
+/** A holder to record instance's capabilities.
+  *
+  * TODO: At the moment it's only a set of Boolean flags, but it could be
+  *       expanded in the future once more features are developed.
+  */
+object Capabilities {
+  var polyglot = false
+}
