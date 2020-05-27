8b73b48efa031545cec3d490907d7f85e4ef65e2
Herdy Handoko <herdy.handoko@gmail.com>
2020-05-27 21:21:49 +0800

    Convert processing to piped methods

--- src/main/scala/com/hhandoko/aktiform/app/controller/FormController.scala
+++ src/main/scala/com/hhandoko/aktiform/app/controller/FormController.scala
@@ -2,6 +2,7 @@ package com.hhandoko.aktiform.app.controller
 
 import java.util.{Map => JMap}
 
+import io.circe.Json
 import org.graalvm.polyglot.Context
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.{
@@ -48,25 +49,46 @@ final class FormController @Autowired() (
   ): String = {
     import scala.jdk.CollectionConverters._
 
-    val filledForm = form(id).fill(data.asScala.toMap)
-    // TODO: Convert to alert and disable form if capability does not exist
-    val result =
-      if (Capabilities.polyglot) {
-        val evaluator = context.eval("js", data.getOrDefault("transform", "x => x"))
-        evaluator.execute(data.get("age").toInt).asInt()
-      } else {
-        data.get("age").toInt
-      }
+    val filledForm  = form(id).fill(data.asScala.toMap)
+    val formPayload = filledForm.toJson
+
+    val printStep     = printKeys(formPayload)
+    val transformStep = transform(printStep)
 
     data.asScala
       .map { case (key, value) => s"$key -> $value" }
       .mkString("<br>")
-      .concat("<br>")
-      .concat(s"id -> $id")
-      .concat("<br>")
-      .concat(s"result -> $result")
-      .concat("<br>")
-      .concat(filledForm.toJson.spaces4SortKeys)
+      .prependedAll("<h1>Form Data</h1>")
+      .prependedAll(s"id -> $id")
+      .prependedAll("<h1>Request</h1>")
+      .concat("<h1>Print Step</h1>")
+      .concat(printStep.spaces4SortKeys)
+      .concat("<h1>Transform Step</h1>")
+      .concat(transformStep.spaces4SortKeys)
+  }
+
+  private def printKeys(payload: Json): Json = {
+    payload.hcursor.downField("data").keys.foreach(println)
+    payload
+  }
+
+  private def transform(payload: Json): Json = {
+    // TODO: Convert to alert and disable form if capability does not exist
+    if (Capabilities.polyglot) {
+      val transformer = payload.hcursor.downField("data").get[String]("transform").getOrElse("x => x")
+      val evaluator   = context.eval("js", transformer)
+      val input       = payload.hcursor.downField("data").get[Int]("age").getOrElse(0)
+      val result      = evaluator.execute(input).asInt()
+
+      payload.hcursor
+        .downField("data")
+        .downField("age")
+        .set(Json.fromInt(result))
+        .top
+        .get
+    } else {
+      payload
+    }
   }
 
   private def form(id: String): Form = {
